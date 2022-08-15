3f164a597daa1d312a1f001221f667c9d6ebf083
Roman <cborzyy@gmail.com>
2022-08-15 13:25:25 +0300

    Add MapView + Compose bottom sheet

---
+++ app/src/main/java/company/vk/education/siriusapp/App.kt
@@ -0,0 +1,13 @@
+package company.vk.education.siriusapp
+
+import android.app.Application
+import com.yandex.mapkit.MapKitFactory
+
+const val MAP_KIT_API_KEY = ""
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MapKitFactory.setApiKey(MAP_KIT_API_KEY)
+    }
+}
--- app/src/main/java/company/vk/education/siriusapp/MainActivity.kt
+++ app/src/main/java/company/vk/education/siriusapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package company.vk.education.siriusapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.ui.viewinterop.AndroidView
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.mapview.MapView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
+
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
+        MapKitFactory.initialize(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mapView = MapView(this)
+        setContent {
+            BottomSheetScaffold(
+                sheetContent = {
+                    Text(text = "Sample text")
+                }
+            ) {
+                AndroidView(factory = { mapView })
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
     }
 }
