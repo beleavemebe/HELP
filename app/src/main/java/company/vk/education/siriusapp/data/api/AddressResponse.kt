e7361492703ebdbaeb45d1ce262721092d020147
Roman <cborzyy@gmail.com>
2022-08-20 18:20:29 +0300

    Use UNKNOWN_LOCATION in EMPTY_ADDRESS

--- app/src/main/java/company/vk/education/siriusapp/data/api/AddressResponse.kt
+++ app/src/main/java/company/vk/education/siriusapp/data/api/AddressResponse.kt
@@ -5,7 +5,7 @@ import company.vk.education.siriusapp.domain.model.Location
 
 class AddressResponse(val name: String, val location: Location) {
     companion object {
-        val EMPTY_ADDRESS = AddressResponse("UNKNOWN", Location(.0, .0))
+        val EMPTY_ADDRESS = AddressResponse("UNKNOWN", Location.LOCATION_UNKNOWN)
     }
 }
 
