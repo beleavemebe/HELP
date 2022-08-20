0799e6eb4b5870a066e4602f3ad10faa273310fc
Roman <cborzyy@gmail.com>
2022-08-20 18:24:55 +0300

    Reformat GeocoderMapper.kt

--- app/src/main/java/company/vk/education/siriusapp/data/api/GeocoderMapper.kt
+++ app/src/main/java/company/vk/education/siriusapp/data/api/GeocoderMapper.kt
@@ -5,9 +5,13 @@ import company.vk.education.siriusapp.domain.model.Location
 import javax.inject.Inject
 
 class GeocoderMapper @Inject constructor() : Mapper<Response, AddressResponse> {
-    override fun map(arg: Response): AddressResponse =
-        arg.response.collection.members.firstOrNull()?.geoObject?.run {
-            val (lon, lat) = point.pos.split(" ").map { it.toDouble() }
-            AddressResponse(name, Location(lat, lon))
-        } ?: AddressResponse.EMPTY_ADDRESS
+    override fun map(arg: Response): AddressResponse {
+        return arg.response.collection.members.firstOrNull()
+            ?.geoObject
+            ?.run {
+                val (lon, lat) = point.pos.split(" ").map { it.toDouble() }
+                AddressResponse(name, Location(lat, lon))
+            }
+            ?: AddressResponse.EMPTY_ADDRESS
+    }
 }
