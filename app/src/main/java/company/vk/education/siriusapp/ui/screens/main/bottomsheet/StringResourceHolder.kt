2470cec417c62bbd987a3cbedd2ad96002396310
Roman <cborzyy@gmail.com>
2022-08-23 11:04:55 +0300

    Delete StringResourceHolder.kt

--- app/src/main/java/company/vk/education/siriusapp/ui/screens/main/bottomsheet/StringResourceHolder.kt
+++
@@ -1,8 +0,0 @@
-package company.vk.education.siriusapp.ui.screens.main.bottomsheet
-
-import androidx.annotation.StringRes
-
-interface StringResourceHolder {
-    @StringRes
-    fun getStringRes(): Int
-}
